8fcce6f8c5861e6968535287481a00d89e959f5a
stallionf1 <ronniezt1987@gmail.com>
2012-12-07 22:19:15 +0200

    added WeeklySupportReport.java

---
+++ src/main/java/com/reporting/WeeklySupportReport.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reporting;
+
+/**
+ *
+ * @author Stallionf1
+ */
+public class WeeklySupportReport {
+    
+}
